c9f815f38cd1b07b7481c6fe28eae151f5d9e5f5
JoyBoy <jprakashchaudhary857@gmail.com>
2025-08-05 10:06:00 +0545

    Doctor Profile Swagger

--- app/Http/Controllers/Api/V1/DoctorProfile/DoctorProfileController.php
+++ app/Http/Controllers/Api/V1/DoctorProfile/DoctorProfileController.php
@@ -49,7 +49,7 @@ class DoctorProfileController extends Controller
      * @OA\Get(
      *     tags={"Patient"},
      *     path="/api/v1/patient/doctor-specialists/{id}",
-     *     summary="Get Doctor List to patient",
+     *     summary="Get Doctro Detail",
      *     @OA\Response(response="200", description="Success"),
      *     security={{"bearerAuth":{}}}
      * )
@@ -69,6 +69,42 @@ class DoctorProfileController extends Controller
             return $this->sendError('Internal Server Error : ' . $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
+
+     /**
+     * @OA\Post(
+     *     path="/api/v1/doctor/doctor-specializations",
+     *     summary="Update the shedule date of the doctor",
+     *     tags={"Doctor"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"experience","qualification","day_of_week","start_time","end_time","slot_duration"},
+     *             @OA\Property(property="experience", type="string", example="2yrs"),
+     *             @OA\Property(property="qualification", type="string", example="Masters if Information Technologies"),
+     *             @OA\Property(property="bio", type="string", example="I am a laravel web developer"),
+     *             @OA\Property(property="consultation_fee", type="integer", example="25000"),
+     *             @OA\Property(property="clinic_name", type="string", example="Gokumldham Social Clinic"),
+     *             @OA\Property(property="clinic_address", type="string", example="Nepal- kathmandu"),
+     *             @OA\Property(property="latitude", type="string", example="192.168.1.1"),
+     *             @OA\Property(property="longitude", type="string", example="192.168.1.1"),
+     *             @OA\Property(property="day_of_week", type="object", example="1,2,3"),
+     *             @OA\Property(property="start_time", type="time", format="time", example="09:00"),
+     *             @OA\Property(property="end_time", type="time", example="15:00"),
+     *             @OA\Property(property="slot_duration", type="string", example="45"),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Update Specification",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="boolean", example=true),
+     *             @OA\Property(property="message", type="string", example="Specification Created/Updated Successfully"),
+     *             @OA\Property(property="data", type="object")
+     *         )
+     *     )
+     * )
+     */
     public function storeOrUpdateSpecialization(DoctorProfileRequest $request)
     {
         DB::beginTransaction();
@@ -104,20 +140,36 @@ class DoctorProfileController extends Controller
 
 
 
+    // public function show($id)
+    // {
+    //     try {
+    //         $profile = DoctorProfile::find($id);
+    //         if (!$profile) {
+    //             return $this->sendError('Doctor Profile not found.', Response::HTTP_NOT_FOUND);
+    //         }
+    //         return $this->sendResponse(new DoctorProfileResource($profile), 'Doctor Profile details fetched.', Response::HTTP_OK);
+    //     } catch (\Exception $e) {
+    //         return $this->sendError('Internal Server Error : ' . $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+    //     }
+    // }
 
-    public function show($id)
-    {
-        try {
-            $profile = DoctorProfile::find($id);
-            if (!$profile) {
-                return $this->sendError('Doctor Profile not found.', Response::HTTP_NOT_FOUND);
-            }
-            return $this->sendResponse(new DoctorProfileResource($profile), 'Doctor Profile details fetched.', Response::HTTP_OK);
-        } catch (\Exception $e) {
-            return $this->sendError('Internal Server Error : ' . $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
-    }
-
+           /**
+     * @OA\Get(
+     *     path="/api/v1/doctor/get-detail",
+     *     summary="Show own Profile",
+     *     tags={"Doctor"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Response(
+     *         response=201,
+     *         description="Shows own profile",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="boolean", example=true),
+     *             @OA\Property(property="message", type="string", example="Profile Fetched Successfully"),
+     *             @OA\Property(property="data", type="object")
+     *         )
+     *     )
+     * )
+     */
 
     public function showOwnProfile()
     {
